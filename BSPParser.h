9690dce95a17595b90b2d0872edbd659130512f1
yogwoggf <100450992+yogwoggf@users.noreply.github.com>
2024-02-01 17:34:29 -0800

    Make spans the primary method of retrieving BSP data

--- BSPParser.h
+++ BSPParser.h
@@ -2,6 +2,7 @@
 
 #include <cstdint>
 #include <stdexcept>
+#include <span>
 
 #include "FileFormat/Parser.h"
 
@@ -251,11 +252,11 @@ public:
 	 */
 	[[nodiscard]] const BSPStructs::Vector* GetVertices() const;
 
-	template<typename C, typename CPtr = std::add_const_t<std::add_pointer_t<C>>>
-	[[nodiscard]] CPtr GetVertices() const
+	template<typename C>
+	[[nodiscard]] std::span<C> GetVertices() const
 	{
-		static_assert(std::is_standard_layout_v<CPtr>, "C must be a standard layout type");
-		return reinterpret_cast<CPtr>(GetVertices());
+		static_assert(std::is_standard_layout_v<C>, "C must be a standard layout type");
+		return std::span<C>(GetVertices(), GetNumVertices());
 	}
 
 	/**
@@ -263,11 +264,11 @@ public:
 	 */
     [[nodiscard]] const BSPStructs::Vector* GetNormals() const;
 
-	template<typename C, typename CPtr = std::add_const_t<std::add_pointer_t<C>>>
-	[[nodiscard]] CPtr GetNormals() const
+	template<typename C>
+	[[nodiscard]] std::span<C> GetNormals() const
 	{
-		static_assert(std::is_standard_layout_v<CPtr>, "C must be a standard layout type");
-		return reinterpret_cast<CPtr>(GetNormals());
+		static_assert(std::is_standard_layout_v<C>, "C must be a standard layout type");
+		return std::span<C>(GetNormals(), GetNumVertices());
 	}
 
 	/**
